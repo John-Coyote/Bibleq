932b06aac6316c69ba84ae2cb5d67c14e3c4f2da
John Nosach <caciquenosach@gmail.com>
2025-05-15 16:56:36 +0600

    prettier config

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
+  const questions = [
+    { nick: "1", name: "question 1", discription: "Discription of question 1" },
+    { nick: "2", name: "question 2", discription: "Discription of question 2" },
+    { nick: "3", name: "question 3", discription: "Discription of question 3" },
+    { nick: "4", name: "question 4", discription: "Discription of question 4" },
+    { nick: "5", name: "question 5", discription: "Discription of question 5" },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <h1>Bibleq</h1>
+      {questions.map((question) => {
+        return (
+          <div key={question.nick}>
+            <h2>{question.name}</h2>
+            <p>{question.discription}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
